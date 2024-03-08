83ddf639ea0e09eeb6efd6361c284b359e6cdfee
Aditya Sawant <adisawant454@gmail.com>
2024-03-08 11:26:36 +0530

    Deleted the extra nested fold

--- Book-Store-Web-backend/routers/router.js
+++
@@ -1,5 +0,0 @@
-const routeItems = require("./items/items");
-const routeAccounts = require("./accounts/account");
-const routeTransactions = require("./tranctions/tranctions");
-
-module.exports = { routeItems, routeAccounts, routeTransactions };
