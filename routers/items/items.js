72e51048d12731aa57c4b322ad6a54487a3f5e98
Aditya Sawant <adisawant454@gmail.com>
2024-03-30 21:36:55 +0530

    fix item.post request

--- routers/items/items.js
+++ routers/items/items.js
@@ -18,53 +18,127 @@ const upload = multer({ storage: storage });
 router
   .route("/")
   .get(async (req, res) => {
+    const { user_email, user_password } = req.query;
     try {
-      const result = await db.query("SELECT * FROM items");
-      res.json(result.rows);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  })
-  .post(async (req, res) => {
-    res.json({ message: "Wellcome to post" });
-  })
-  .put(async (req, res) => {
-    const { item_id, reduced_quantity } = req.body;
-    try {
-      console.log({ item_id: item_id, reduced_quantity: reduced_quantity });
-      const item_query = await db.query(
-        "SELECT * FROM items  WHERE item_id = $1",
-        [item_id]
-      );
-      console.log(item_query.rows[0]);
-      if (item_query.rows[0].item_quantity >= reduced_quantity) {
-        const quantity_after_reduction =
-          parseInt(item_query.rows[0].item_quantity) -
-          parseInt(reduced_quantity);
-        console.log(quantity_after_reduction);
-        const result = await db.query(
-          "UPDATE items SET item_quantity = $2 WHERE item_id = $1 RETURNING *",
-          [item_id, quantity_after_reduction]
+      if (user_email) {
+        const user = await db.query(
+          "SELECT * FROM account WHERE email = $1 AND password = $2",
+          [user_email, user_password]
         );
-        console.log(result.rows[0]);
-        res.json({
-          success: true,
-          message: "quantity updated",
-          data: result.rows[0],
-        });
+        if (user.rows.length > 0) {
+          const user_items = await db.query(
+            "SELECT * FROM items WHERE item_seller_id = $1",
+            [user.rows[0].user_id]
+          );
+          res.json(user_items.rows);
+        } else {
+          res.status(404).json({ error: "User not found" });
+        }
       } else {
-        res.status(304).json({
-          success: false,
-          message: "quantity not updated",
-          data: item_query.rows[0],
-        });
+        const result = await db.query("SELECT * FROM items");
+        res.json(result.rows);
       }
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
   })
+  .post(async (req, res) => {
+    const {
+      item_name,
+      item_description,
+      item_price,
+      item_category,
+      item_rating,
+      item_reviews,
+      item_image_url,
+      item_seller_id,
+      item_quantity,
+    } = req.body;
+
+    try {
+      const result = await db.query(
+        " INSERT INTO items (item_name, item_description, item_price, item_category, item_rating, item_reviews, item_image_url, item_seller_id, item_quantity) VALUES ( $1, $2, $3, $4, $5, $6, $7,$8, $9 ) RETURNING *;",
+        [
+          item_name,
+          item_description,
+          item_price,
+          item_category,
+          item_rating,
+          item_reviews,
+          item_image_url,
+          item_seller_id,
+          item_quantity,
+        ]
+      );
+
+      res.json(result.rows);
+    } catch (error) {
+      res.json({ message: "item post", error: error.message });
+    }
+  })
+  .put(async (req, res) => {
+    const {
+      item_id,
+      item_name,
+      item_description,
+      item_price,
+      item_category,
+      item_rating,
+      item_reviews,
+      item_image_url,
+      item_seller_id,
+      item_quantity,
+    } = req.body;
+    try {
+      const result = await db.query(
+        "UPDATE items SET item_name = $1, item_description = $2, item_price = $3, item_category = $4, item_rating = $5, item_reviews = $6, item_image_url = $7, item_seller_id = $8, item_quantity = $9 WHERE item_id = $10 RETURNING *;",
+        [
+          item_name,
+          item_description,
+          item_price,
+          item_category,
+          item_rating,
+          item_reviews,
+          item_image_url,
+          item_seller_id,
+          item_quantity,
+          item_id,
+        ]
+      );
+
+      console.log(result.rows[0]);
+      res.json({
+        success: true,
+        message: "item updated",
+        data: result.rows,
+      });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  })
   .delete(async (req, res) => {
-    res.json({ message: "Wellcome to delete" });
+    const { item_id } = req.body;
+    try {
+      const result = await db.query(
+        "DELETE FROM items WHERE item_id = $1 RETURNING *;",
+        [item_id]
+      );
+      if (result.rows.length !== 0) {
+        res.json({
+          success: true,
+          message: "item deleted",
+          data: result.rows,
+        });
+      } else {
+        res.json({
+          success: false,
+          message: "item not found",
+          data: result.rows,
+        });
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
   });
 module.exports = router;
 
